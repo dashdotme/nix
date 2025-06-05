bb4001fe792bf5518bc1152410dd52d186f2feb1
Dash Vallance <dash@intelics.com.au>
2025-06-05 20:17:49 +1000

    home: battle nvidia suspend settings

--- cfg-home-only.nix
+++ cfg-home-only.nix
@@ -6,7 +6,7 @@
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     modesetting.enable = true;
-    powerManagement.enable = false;
+    powerManagement.enable = true;
     powerManagement.finegrained = false;
     nvidiaSettings = true;
     open = false;
@@ -18,4 +18,11 @@
     "intel_idle.max_cstate=1"
   ];
 
+  systemd.services.systemd-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+
+  systemd.services."systemd-suspend" = {
+    serviceConfig = {
+      Environment=''"SYSTEMD_SLEEP_FREEZE_USER_SESSIONS=false"'';
+    };
+  };
 }
